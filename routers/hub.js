0861b65784e1836351156b6a10b404bf8b89e2bc
raythurman2386 <raymondthurman5@gmail.com>
2019-12-05 21:33:10 -0500

    lecture final

---
+++ routers/hub.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const hubs = require("../hubs/hubs-model.js");
+const messageRouter = require("./message");
+
+const router = express.Router();
+
+router.use("/:id/messages", messageRouter);
+
+router.get("/", (req, res) => {
+  console.log(req.query);
+  const opts = {
+    limit: req.query.limit,
+    sortby: req.query.sortby,
+    sortdir: req.query.sortdir
+  };
+  hubs
+    .find(opts)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hubs"
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  hubs
+    .findById(req.params.id)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "Hub not found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the hub"
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing hub name" });
+  }
+
+  hubs
+    .add(req.body)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the hub"
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({ message: "Missing hub name" });
+  }
+
+  hubs
+    .update(req.params.id, req.body)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the hub"
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  hubs
+    .remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The hub has been nuked" });
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub"
+      });
+    });
+});
+
+module.exports = router;
---
+++ routers/message.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hubs = require("../hubs/hubs-model.js");
+
+const router = express.Router({
+  mergeParams: true
+});
+
+router.get("/", (req, res) => {
+  hubs
+    .findHubMessages(req.params.id)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json({ message: "No messages" }));
+});
+
+router.get("/:messageId", (req, res) => {
+  hubs
+    .findHubMessageById(req.params.id, req.params.messageId)
+    .then(data => {
+      if (data) {
+        res.json(data);
+      } else {
+        res.status(404).json({
+          message: "Message not found"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ message: "No messages" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const body = {
+    sender: req.body.sender,
+    text: req.body.text
+  };
+  hubs
+    .addHubMessage(req.params.id, body)
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(400).json({ message: "No messages" });
+    });
+});
+
+module.exports = router;
